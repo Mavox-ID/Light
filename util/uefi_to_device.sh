6511230a5b0f6d8f02156444241d6a3a9bc4d41b
Mavox-ID <markd.voznyuk@gmail.com>
2026-05-17 11:54:11 +0300

    Update: util/uefi_to_device.sh

---
+++ util/uefi_to_device.sh
@@ -0,0 +1,37 @@
+# Run as root.
+# Pass device file of ESP as first argument.
+# Pass device file of LightFS partition as second argument.
+
+set -e
+
+util/uefi_compile.sh
+
+mkdir -p mount
+mount $1 mount
+mkdir -p mount/EFI/BOOT
+cp bin/uefi mount/EFI/BOOT/BOOTX64.EFI
+cp bin/Kernel.lpp mount/eskernel.lpp
+cp bin/uefi_loader mount/esloader.bin
+
+if [ $# -eq 2 ]; then
+	cp bin/iid.dat mount/esiid.dat
+fi
+
+umount $1
+rmdir mount
+
+if [ $# -eq 1 ]; then
+	echo Skipping drive copy.
+	exit 0
+fi
+
+SOURCE_OFFSET=`fdisk -l bin/drive | grep 'Linux' | awk '{print $3}'`
+SOURCE_COUNT=`fdisk -l bin/drive | grep 'Linux' | awk '{print $5}'`
+DESTINATION_COUNT=`blockdev --getsz $2`
+
+if [ "$SOURCE_COUNT" -gt "$DESTINATION_COUNT" ]; then
+	echo Please set Emulator.PrimaryDriveMB to fit on the drive.
+	exit 1
+fi
+
+dd if=bin/drive of=$2 bs=512 count=$SOURCE_COUNT skip=$SOURCE_OFFSET conv=notrunc
